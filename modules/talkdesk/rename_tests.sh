6ff524571c9f860eb81b52822524661e2be71ad7
Carlos Fonseca <carlosefonseca@gmail.com>
2021-04-21 12:29:54 +0100

    Samll rename_tests.sh improv

--- modules/talkdesk/rename_tests.sh
+++ modules/talkdesk/rename_tests.sh
@@ -9,6 +9,6 @@ R4='s/test_should_(\w+)_if_(\w+)\(/test_WHEN_$2_THEN_$1(/'
 R5='s/test_that_when_(\w+)_then_/test_WHEN_$1_THEN_/'
 R6='s/test_it_should_(\w+)_if_(\w+)\(/test_WHEN_$2_THEN_should_$1(/'
 R7='s/test_given_(\w+)_when_(\w+)_then_(\w+)/test_GIVEN_$1_WHEN_$2_THEN_$3/'
-R8='s/_i_am_/_I_am_/'
+R8='s/_i_/_I_/g '
 
 perl -pi -e "$R1 ; $R2 ; $R3 ; $R4 ; $R5 ; $R6 ; $R7 ; $R8" $1
