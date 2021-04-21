6d5e4f89b509055e39fad8337f09989d3f4bd369
Carlos Fonseca <carlosefonseca@gmail.com>
2021-04-21 12:29:23 +0100

    git prune correctly updates develop

--- modules/talkdesk/git_prune_local_branches_without_tracking.sh
+++ modules/talkdesk/git_prune_local_branches_without_tracking.sh
@@ -3,5 +3,7 @@
 git fetch --prune && git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
 
 if (git branch | grep develop); then
-  git branch -f develop origin/develop
+
+  $(dirname $0)/git_force_reset_to_tracking.sh develop
+
 fi
